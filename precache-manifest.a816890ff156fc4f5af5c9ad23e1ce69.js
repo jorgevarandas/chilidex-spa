c467e7d84ce618fe6bca57e32479f3ebf4d6f8f2
Jorge Varandas <jorge.varandas@mercedes-benz.io>
2019-01-24 23:08:32 +0000

    kickstart

---
+++ precache-manifest.a816890ff156fc4f5af5c9ad23e1ce69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3fb3f0835e6481ed2e42",
+    "url": "/static/css/main.c55a933f.chunk.css"
+  },
+  {
+    "revision": "3fb3f0835e6481ed2e42",
+    "url": "/static/js/main.3fb3f083.chunk.js"
+  },
+  {
+    "revision": "70d8535c3a30cba55802",
+    "url": "/static/js/1.70d8535c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c474a01ca392c0d107ceed15246234c4",
+    "url": "/index.html"
+  }
+];
